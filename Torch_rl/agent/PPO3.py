d8a44010029076264c57582077f4cc1e13d46225
Zee-ubuntu <zachary2wave@163.com>
2020-03-30 22:57:13 +0800

    bug fix

--- Torch_rl/agent/PPO3.py
+++ Torch_rl/agent/PPO3.py
@@ -9,7 +9,7 @@ from torch.optim import Adam
 from torch.autograd import Variable
 from gym import spaces
 from Torch_rl.common.util import csv_record
-from Torch_rl.common.util import gae
+from Torch_rl.common.util import get_gae
 
 class gpu_foward(nn.Module):
     def __init__(self, model):
@@ -19,13 +19,14 @@ class gpu_foward(nn.Module):
     def forward(self,obs):
         obs = obs.cuda()
         out = self.model(obs)
+        del obs
         return out
 
 class PPO_Agent(Agent_policy_based):
     def __init__(self, env, policy_model, value_model,
                  lr=5e-4, ent_coef=0.01, vf_coef=0.5,
                  ## hyper-parawmeter
-                 gamma=0.90, lam=0.95, cliprange=0.2, batch_size=64, value_train_round=10,
+                 gamma=0.99, lam=0.95, cliprange=0.2, batch_size=64, value_train_round=10,
                  running_step=2048, running_ep=20, value_regular=0.01,
                  ## decay
                  decay=False, decay_rate=0.9,
@@ -90,7 +91,7 @@ class PPO_Agent(Agent_policy_based):
         loss_re, pgloss_re, enloss_re, vfloss_re = [], [], [], []
 
         for key in sample.keys():
-            temp = torch.stack(list(sample[key])).squeeze()
+            temp = torch.stack(list(sample[key]), 0).squeeze()
             if self.gpu:
                 sample[key] = temp.cuda()
             else:
@@ -121,14 +122,14 @@ class PPO_Agent(Agent_policy_based):
             vf_loss = .5 * torch.max(vf_loss1, vf_loss2)
 
             #generate Policy gradient loss
-            outcome = self.policy.forward(training_s)
+            outcome = self.policy.forward(training_s).squeeze()
             # new_neg_lop = torch.empty(size=(self.batch_size,))
             # for time in range(self.batch_size):
             #     new_policy = self.dist(outcome[time])
             #     new_neg_lop[time] = new_policy.log_prob(training_a[time])
             new_policy = self.dist(outcome)
             new_neg_lop = new_policy.log_prob(training_a)
-            ratio = torch.exp(torch.diag(new_neg_lop) - old_neglogp)
+            ratio = torch.exp(new_neg_lop - old_neglogp)
             pg_loss1 = -advs * ratio
             pg_loss2 = -advs * torch.clamp(ratio, 1.0 - self.cliprange, 1.0 + self.cliprange)
             pg_loss = .5 * torch.max(pg_loss1, pg_loss2).mean()
@@ -153,25 +154,65 @@ class PPO_Agent(Agent_policy_based):
                 self.value_model_optim.step()
             # approxkl = self.loss_cal(neg_log_pac, self.record_sample["neglogp"])
             # self.cliprange = torch.gt(torch.abs(ratio - 1.0).mean(), self.cliprange)
-            loss_re = loss.detach().numpy()
-            pgloss_re.append(pg_loss.detach().numpy())
-            enloss_re.append(entropy.detach().numpy())
-            vfloss_re.append(vf_loss1.detach().numpy())
+            loss_re = loss.cpu().detach().numpy()
+            pgloss_re.append(pg_loss.cpu().detach().numpy())
+            enloss_re.append(entropy.cpu().detach().numpy())
+            vfloss_re.append(vf_loss1.cpu().detach().numpy())
         return np.sum(loss_re), {"pg_loss": np.sum(pgloss_re),
                                    "entropy": np.sum(enloss_re),
                                    "vf_loss": np.sum(vfloss_re)}
 
 
     def load_weights(self, filepath):
-        model = torch.load(filepath+"ppo.pkl")
+        model = torch.load(filepath+"/PPO.pkl")
         self.policy.load_state_dict(model["policy"].state_dict())
         self.value.load_state_dict(model["value"].state_dict())
 
 
     def save_weights(self, filepath, overwrite=False):
-        torch.save({"policy": self.policy,"value": self.value}, filepath + "PPO.pkl")
+        torch.save({"policy": self.policy,"value": self.value}, filepath + "/PPO.pkl")
+
+    def policy_behavior_clone(self, sample_):
+        action_label = sample_["a"].squeeze()
+        if self.gpu:
+            action_predict = self.policy(sample_["s"].cuda())
+            action_label = action_label.cuda()
+        else:
+            action_predict = self.policy(sample_["s"])
+        loss_bc = self.loss_cal(action_label, action_predict)
+        del action_label
+        del action_predict
+        loss = loss_bc
+        self.policy_model_optim.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(self.policy.parameters(), 1, norm_type=2)
+        self.policy_model_optim.step()
+        return loss.cpu().detach().numpy()
+
+    def value_pretrain(self, record_sample, new_sample_len):
+        train_times = int(np.floor(new_sample_len/128))
+        round_loss = 0
+        for io in range(train_times-1):
+            index = list(range(128 * io, 128 * (io + 1)))
+            if self.gpu:
+                predict = torch.from_numpy(np.array(record_sample["s"])[index]).cuda()
+                lable = torch.from_numpy(np.array(record_sample["return"]))[index].cuda()
+            else:
+                predict = torch.from_numpy(np.array(record_sample["s"])[index])
+                lable = torch.from_numpy(np.array(record_sample["return"]))[index]
+            value_now = self.value.forward(predict)
+            # value loss
+            vf_loss = self.loss_cal(value_now, lable)  # Unclipped loss
+            del predict
+            del lable
+            self.value_model_optim.zero_grad()
+            vf_loss.backward()
+            self.value_model_optim.step()
+            round_loss += vf_loss.cpu().detach().numpy()
+        return round_loss
 
     def cuda(self):
         self.policy = gpu_foward(self.policy)
         self.value = gpu_foward(self.value)
+        self.loss_cal = self.loss_cal.cuda()
         self.gpu = True
